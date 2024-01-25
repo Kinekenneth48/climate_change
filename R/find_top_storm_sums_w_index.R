c2e068c2abf6f4b46a708171d31982220bfeef97
Kinekenneth48 <Kinekenneth48@gmail.com>
2024-01-25 14:47:18 -0600

    extract storm vars for swe model

--- R/find_top_storm_sums_w_index.R
+++ R/find_top_storm_sums_w_index.R
@@ -3,15 +3,15 @@ find_top_storm_sums_w_index <- function(data, num_zeros) {
   storm_sums <- numeric()
   start_indices <- numeric()
   end_indices <- numeric()
-  
+
   # Start index of the first storm
   start_index <- 1
-  
+
   # Iterate over the data to extract storms
   for (i in 1:(length(data) - 1)) {
-    # Check for a sequence of num_zeros zeros to mark the end of a storm
-    if (i < (length(data) - num_zeros + 1) && all(data[i:(i + num_zeros - 1)] == 0, na.rm = TRUE)) {
-      # Calculate storm sum, skipping over zero-only segments
+    if (i < (length(data) - num_zeros + 1) && all(data[i:(i + num_zeros - 1)] == 0,
+      na.rm = TRUE
+    )) {
       end_index <- i - 1
       if (start_index <= end_index) {
         storm_sum <- sum(data[start_index:end_index], na.rm = TRUE)
@@ -19,30 +19,36 @@ find_top_storm_sums_w_index <- function(data, num_zeros) {
         start_indices <- c(start_indices, start_index)
         end_indices <- c(end_indices, end_index)
       }
-      # Update the start index for the next storm
       start_index <- i + num_zeros
     }
   }
-  
-  # Check and sum the last segment if it doesn't end with zeros
+
   if (start_index <= length(data)) {
     storm_sum <- sum(data[start_index:length(data)], na.rm = TRUE)
     storm_sums <- c(storm_sums, storm_sum)
     start_indices <- c(start_indices, start_index)
     end_indices <- c(end_indices, length(data))
   }
-  
+
+  # Ensure there are at least 3 storms by adding placeholder values
+  while (length(storm_sums) < 3) {
+    storm_sums <- c(storm_sums, 0)
+    start_indices <- c(start_indices, 1)
+    end_indices <- c(end_indices, length(data))
+  }
+
   # Sorting and selecting top 3 storms
   top_storms_indices <- order(storm_sums, decreasing = TRUE)[1:3]
-  top_storms <- cbind(storm_sums[top_storms_indices], start_indices[top_storms_indices], end_indices[top_storms_indices])
-  
-  # Flatten and replace NA with 0
-  top_storms_vector <- as.vector(t(top_storms))
-  top_storms_vector[is.na(top_storms_vector)] <- 0
-  
-  #return storm sum, start index, end index;storm sum, start index, end index (in that order)
-  return(top_storms_vector)
+  top_storms <- cbind(
+    storm_sums[top_storms_indices],
+    start_indices[top_storms_indices],
+    end_indices[top_storms_indices]
+  )
+
+  # Flatten and return the vector
+  return(as.vector(t(top_storms)))
 }
+
 
 # # # Test the function
 # storm_data <- c(1, 2, 3,NA,  4, 5, 0, 0, 6, 7, 8, 9,  10, 11)
--- script/swe_model/get_swe_model_variables_w_storms.R
+++ script/swe_model/get_swe_model_variables_w_storms.R
@@ -7,12 +7,11 @@
 # ============================================================================#
 library(terra)
 
-source("R/zero_position_before_max.R")
-source("R/cal_max_cons_days_below_thrd.R")
-source("R/cal_max_cons_days_above_thrd.R")
+source("R/find_top_storm_sums_w_index.R")
 
-#manage memory usage so PC doesn't crash
-terraOptions(memfrac = 0.80, verbose = TRUE) 
+
+# manage memory usage so PC doesn't crash
+terraOptions(memfrac = 0.80, verbose = TRUE)
 
 # ============================================================================#
 # load data
@@ -47,4 +46,228 @@ temp_threshold <- 276.15
 # loca_pr <- loca_pr * 86400
 # tictoc::toc()
 
+################################################################################
+## STEP 1: get max annual snow and corresponding variables
+################################################################################
 
+# Extracting years and months from the time attributes
+years_loca_tas <- as.numeric(format(time(loca_tas), "%Y"))
+months_loca_tas <- as.numeric(format(time(loca_tas), "%m"))
+
+# Identify the unique years in your dataset
+unique_years <- unique(years_loca_tas)
+
+# Initialize vectors to store the maximum SWE and corresponding temperature
+max_swe_list <- vector("list", length = length(unique_years) - 1)
+temp_diff_list <- vector("list", length = length(unique_years) - 1)
+mean_temp_list <- vector("list", length = length(unique_years) - 1)
+total_ppt_list <- vector("list", length = length(unique_years) - 1)
+snow_window_list <- vector("list", length = length(unique_years) - 1)
+
+storm_one_temp_list <- vector("list", length = length(unique_years) - 1)
+storm_two_temp_list <- vector("list", length = length(unique_years) - 1)
+storm_three_temp_list <- vector("list", length = length(unique_years) - 1)
+
+storm_one_list <- vector("list", length = length(unique_years) - 1)
+storm_two_list <- vector("list", length = length(unique_years) - 1)
+storm_three_list <- vector("list", length = length(unique_years) - 1)
+
+
+# Initialize a progress bar
+pb <- txtProgressBar(min = 0, max = length(unique_years) - 1, style = 3)
+
+
+tictoc::tic() # 7 hrs
+# Loop through each water year (October to September next year)
+for (j in 1:(length(unique_years) - 1)) {
+  # Update the progress bar
+  setTxtProgressBar(pb, j)
+
+  start_year <- unique_years[j]
+  end_year <- unique_years[j + 1]
+
+  # Create a mask for the water year (October of start_year to September of end_year)
+  water_year_mask <- (years_loca_tas == start_year & months_loca_tas >= 10) |
+    (years_loca_tas == end_year & months_loca_tas <= 9)
+
+  # Create a mask for the water year (winter months)
+  water_year_mask_winter <- (years_loca_tas == start_year & months_loca_tas >= 12) |
+    (years_loca_tas == end_year & months_loca_tas <= 3)
+
+  # Subset the data for water year and convert ppt from kg m2 s to mm
+  swe_subset <- ua_swe[[which(water_year_mask)]]
+  temp_subset <- loca_tas[[which(water_year_mask)]]
+  pr_subset <- loca_pr[[which(water_year_mask)]]
+
+  # Subset the data for water year(winter) and convert ppt from kg m2 s to mm
+  temp_subset_winter <- loca_tas[[which(water_year_mask_winter)]]
+  pr_subset_winter <- loca_pr[[which(water_year_mask_winter)]]
+
+  # Check: Ensure that all subsetted rasters have the same number of layers
+  if (nlyr(swe_subset) != nlyr(temp_subset) || nlyr(swe_subset) != nlyr(pr_subset)) {
+    stop("Mismatch in number of layers for year: ", start_year)
+  }
+
+
+  # if temperature is below the threshold, keep the precipitation value (as snow),
+  # else set to zero (15 mins)
+  modified_pr_subset <- terra::ifel(temp_subset < temp_threshold, pr_subset, 0)
+  modified_pr_subset_winter <- terra::ifel(
+    temp_subset_winter < temp_threshold,
+    pr_subset_winter, 0
+  )
+
+
+  # ============================================================================#
+  # 1. annual max SWE
+  # ============================================================================#
+  max_swe <- max(swe_subset, na.rm = TRUE)
+
+  # ============================================================================#
+  # 2. winter temp difference
+  # ============================================================================#
+  temp_diff <- max(temp_subset_winter, na.rm = TRUE) - min(temp_subset_winter,
+    na.rm = TRUE
+  )
+
+  # ============================================================================#
+  # 3. winter temp mean
+  # ============================================================================#
+  mean_temp <- mean(temp_subset_winter, na.rm = TRUE)
+
+  # ============================================================================#
+  # 4. get total ppt in winter months
+  # ============================================================================#
+  total_ppt <- sum(modified_pr_subset_winter, na.rm = TRUE)
+
+  # ============================================================================#
+  # 5. MAX OF 21 SNOW WINDOW
+  # ============================================================================#
+  snow_window <- max(terra::roll(
+    x = modified_pr_subset_winter,
+    n = 21, fun = sum, type = "around", na.rm = TRUE
+  ))
+
+  # ============================================================================#
+  # 6. get top 3 storms by summation
+  # num_zeros=3 means two storms separated by three zeros will be one storm
+  # ============================================================================#
+  storm <- app(modified_pr_subset, fun = find_top_storm_sums_w_index, 
+               num_zeros = 3)
+
+  storm_one <- storm[[1]]
+  storm_two <- storm[[4]]
+  storm_three <- storm[[7]]
+
+  # ============================================================================#
+  # 7. get top 3 storms mean temp
+  # ============================================================================#
+  storm_one_temp <- terra::rapp(
+    x = temp_subset, first = storm[[2]],
+    last = storm[[3]], fun = "mean"
+  )
+
+  storm_two_temp <- terra::rapp(
+    x = temp_subset, first = storm[[5]],
+    last = storm[[6]], fun = "mean"
+  )
+
+  storm_three_temp <- terra::rapp(
+    x = temp_subset, first = storm[[8]],
+    last = storm[[9]], fun = "mean"
+  )
+
+
+  # Store the max SWE and corresponding temp layer in the lists
+  # and convert ppt from kg m2 s to mm
+  max_swe_list[[j]] <- max_swe
+  temp_diff_list[[j]] <- temp_diff
+  mean_temp_list[[j]] <- mean_temp
+  total_ppt_list[[j]] <- total_ppt * 86400
+  snow_window_list[[j]] <- snow_window * 86400
+
+  storm_one_temp_list[[j]] <- storm_one_temp
+  storm_two_temp_list[[j]] <- storm_two_temp
+  storm_three_temp_list[[j]] <- storm_three_temp
+  storm_one_list[[j]] <- storm_one * 86400
+  storm_two_list[[j]] <- storm_two * 86400
+  storm_three_list[[j]] <- storm_three * 86400
+
+}
+# Close the progress bar
+close(pb)
+
+tictoc::toc()
+
+
+
+# Convert the lists to SpatRaster (multi-layer raster)
+max_swe_layers <- terra::rast(max_swe_list)
+temp_diff_layers <- terra::rast(temp_diff_list)
+mean_temp_layers <- terra::rast(mean_temp_list)
+total_ppt_layers <- terra::rast(total_ppt_list)
+snow_window_layers <- terra::rast(snow_window_list)
+
+storm_one_temp_layers <- terra::rast(storm_one_temp_list)
+storm_two_temp_layers <- terra::rast(storm_two_temp_list)
+storm_three_temp_layers <- terra::rast(storm_three_temp_list)
+storm_one_layers <- terra::rast(storm_one_list)
+storm_two_layers <- terra::rast(storm_two_list)
+storm_three_layers <- terra::rast(storm_three_list)
+
+
+# Save the resulting multi-layer raster layers
+writeRaster(max_swe_layers,
+            "data-raw/swe_model_vars/max_swe_layers.tif",
+            overwrite = TRUE
+)
+
+writeRaster(temp_diff_layers,
+            "data-raw/swe_model_vars/temp_diff_layers.tif",
+            overwrite = TRUE
+)
+
+writeRaster(mean_temp_layers,
+            "data-raw/swe_model_vars/mean_temp_layers.tif",
+            overwrite = TRUE
+)
+
+writeRaster(total_ppt_layers,
+            "data-raw/swe_model_vars/total_ppt_layers.tif",
+            overwrite = TRUE
+)
+
+
+writeRaster(snow_window_layers,
+            "data-raw/swe_model_vars/snow_window_layers.tif",
+            overwrite = TRUE
+)
+
+
+writeRaster(storm_one_temp_layers,
+            "data-raw/swe_model_vars/storm_one_temp_layers.tif",
+            overwrite = TRUE
+)
+
+writeRaster(storm_two_temp_layers,
+            "data-raw/swe_model_vars/storm_two_temp_layers.tif",
+            overwrite = TRUE
+)
+writeRaster(storm_three_temp_layers,
+            "data-raw/swe_model_vars/storm_three_temp_layers.tif",
+            overwrite = TRUE
+)
+
+writeRaster(storm_one_layers,
+            "data-raw/swe_model_vars/storm_one_layers.tif",
+            overwrite = TRUE
+)
+
+writeRaster(storm_two_layers,
+            "data-raw/swe_model_vars/storm_two_layers.tif",
+            overwrite = TRUE
+)
+writeRaster(storm_three_layers,
+            "data-raw/swe_model_vars/storm_three_layers.tif",
+            overwrite = TRUE
+)
